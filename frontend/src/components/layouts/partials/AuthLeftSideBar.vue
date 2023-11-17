849cfd0a3fd4c33342f3e57e195a78843f99cca8
Shaikh Al Amin <alamin@tikweb.com>
2023-11-17 14:26:38 +0600

    Design issue fixing

--- frontend/src/components/layouts/partials/AuthLeftSideBar.vue
+++ frontend/src/components/layouts/partials/AuthLeftSideBar.vue
@@ -9,45 +9,25 @@
         </b-nav-item>
         <b-nav-item class="mt-3" @click="redirectToHome()">
           <span>
-            <b-icon icon="house-door-fill" scale="1.2" variant="dark"></b-icon
-          ></span>
+            <b-icon icon="house-door-fill" scale="1.2" variant="dark"></b-icon></span>
           <span class="sidebar-menu"> Home</span>
         </b-nav-item>
         <b-nav-item class="" @click="redirectToProfile()">
           <span>
-            <b-icon icon="person" scale="1.2" variant="dark"></b-icon
-          ></span>
+            <b-icon icon="person" scale="1.2" variant="dark"></b-icon></span>
           <span class="sidebar-menu"> Profile</span>
         </b-nav-item>
         <b-nav-item>
-          <b-row>
-            <b-col sm="2">
-              <b-img
-                fluid
-                rounded="circle"
-                :src="tokenUser?.user?.avatar"
-                :alt="tokenUser?.user?.name"
-              ></b-img>
-            </b-col>
-            <b-col sm="10">
-              <div>
-                <div class="ft-16 text-dark">
-                  <span class="fw-500">{{ tokenUser?.user?.name }}</span> @{{
-                    tokenUser?.user?.username
-                  }}
-                </div>
-              </div>
-            </b-col>
-          </b-row>
+          <span>
+            <img :src="tokenUser?.user?.avatar" :alt="tokenUser?.user?.name" width="20" height="20" class="rounded" />
+            </span>
+          <span class="ft-16 text-dark ml-10"> <span class="fw-500">{{ tokenUser?.user?.name }}</span> @{{
+            tokenUser?.user?.username
+          }}</span>
         </b-nav-item>
         <b-nav-item @click="logOut()">
           <span>
-            <b-icon
-              icon="box-arrow-in-right"
-              scale="1.2"
-              variant="dark"
-            ></b-icon
-          ></span>
+            <b-icon icon="box-arrow-in-right" scale="1.2" variant="dark"></b-icon></span>
           <span class="sidebar-menu"> Logout</span>
         </b-nav-item>
       </b-nav>
@@ -70,18 +50,18 @@ export default {
     redirectToProfile: function () {
       this.$router
         .push(`/profile/${this.tokenUser.user.username}`)
-        .catch(() => {})
+        .catch(() => { })
       this.$router.go(0)
     },
     redirectToHome: function () {
-      this.$router.push('/timeline').catch(() => {})
+      this.$router.push('/timeline').catch(() => { })
       this.$router.go(0)
     },
     logOut () {
       authLogOut()
         .then(() => {
           removeLocalSession()
-          this.$router.push('/').catch(() => {})
+          this.$router.push('/').catch(() => { })
         })
         .catch((err) => {
           console.log('err', err.message)
